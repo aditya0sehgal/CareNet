4d277702a32b13993cffb6e78a1250b7f78b87bc
adityak14 <aditya.kaushik1213@gmail.com>
2021-01-30 22:23:16 +0530

    Added MongoDB Authentication

--- api/api.py
+++ api/api.py
@@ -17,13 +17,20 @@ import tensorflow as tf
 from keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.models import load_model
 from keras.preprocessing import image
-# from keras.preprocessing.image import load_image, image_to_array
 from werkzeug.utils import secure_filename
-
-
 from flask_cors import CORS
 
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+#db
+from flask import Flask, render_template, redirect, url_for, flash, session, request, g
+import pymongo
+from pymongo import MongoClient
+from flask_login import LoginManager
+from werkzeug.security import generate_password_hash,check_password_hash
+import bcrypt
+from flask_login import login_user,current_user
+from PIL import Image , ImageOps
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 #Pneumonia Model
 MODEL_PATH = 'pneumonia.h5'
@@ -34,20 +41,24 @@ diabetesLoadedModel, means, stds = joblib.load('diabetesModel.pkl')
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/uploads'
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 CORS(app, expose_headers='Authorization')
 
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(64, 64)) #target_size must agree with what the trained model expects!!
+#Database
+client=MongoClient('mongodb+srv://test:test@cluster0.8dn6j.mongodb.net/<dbname>?retryWrites=true&w=majority')
+db=client['Users']
+det=[1,3]
 
+from PIL import Image
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(64,64)) #target_size must agree with what the trained model expects!!
+    # img = ImageOps.grayscale(img) 
     # Preprocessing the image
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
     preds = model.predict(img)
     return preds
   
-
-
-
 @app.route('/pneumonia-predict', methods=['POST','GET'])
 def fileUpload():
     result = ''
@@ -183,6 +194,103 @@ def healthscore():
 #             return str2
 #     return None
 
+@app.route('/main')
+def home1():
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():    
+    return render_template('login.html')
+    
+
+
+@app.route('/login',methods=['POST'])
+def login_post():
+    # session.pop('user',None)
+    email=request.form.get('email')
+    password=request.form.get('password')
+    data=db.credentials
+    flag=False    
+    # session.pop('User',None)      
+    print(email, password)
+    if email is None or password is None:
+        flash("Please fill both the fields and Try Again")
+        return redirect(url_for('login'))
+    
+    
+    for i in data.find():
+        if i['email']==email:
+            name=i['username']
+            print(i, password, generate_password_hash(password, method='sha256'))            
+            if check_password_hash(i['password'], password):    
+                flag=True
+                session['user']=name 
+                print(session['user'])  
+                det[0]=name
+                det[1]=email                                  
+                return redirect(url_for('profile'))
+            
+    if flag==False:
+        flash("Invalid Credentials")
+        return redirect(url_for('login'))
+    
+    return redirect(url_for('login'))
+   
+
+    
+
+@app.route('/register')
+def reg():
+    return render_template('register.html')
+
+@app.route('/register',methods=['POST'])
+def reg_post():
+    name=request.form.get('name')
+    email=request.form.get('email')
+    mob=request.form.get('mobile')
+    password=request.form.get('password')
+    print(email, password)
+    password=generate_password_hash(password, method='sha256')
+    
+    
+    data=db.credentials
+    for i in data.find():
+        if i['email']==email:  
+            flash("Your Email Address is already registered with us")
+            return redirect(url_for('login')) 
+    else:   
+        user_info={'username': name,
+                'mobile':mob,
+                'email': email,
+                'password': password} 
+        data.insert_one(user_info)
+        
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logoute():
+    session.pop('user', None)  
+    det=[]  
+    return redirect(url_for('login'))
+
+@app.route('/profile')
+def profile():   
+    if g.user:
+         return render_template('profile.html',username=det[0],email=det[1])        
+    elif det==[]:
+        flash("You are kindly requested to Login first")        
+        return redirect(url_for('login')) 
+   
+    flash("You are kindly requested to Login first") 
+    return redirect(url_for('login'))
+
+@app.before_request
+def before_request():
+    g.user=None
+    det=[]
+    if 'user' in session:
+        g.user=session['user']
 
 
 
