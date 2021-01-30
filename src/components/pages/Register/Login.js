c7d70435df6ee233ddbc39a4d58c7eb31fecc3c8
adityak14 <aditya.kaushik1213@gmail.com>
2021-01-30 21:50:56 +0530

    Login And Register page

--- src/components/pages/Register/Login.js
+++ src/components/pages/Register/Login.js
@@ -1,34 +1,66 @@
 import React, { Component } from 'react'
 import './style.css'
 class Login extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={ };
-    }
-    submitlogin(e){
-
-    }
+        this.state = {
+            result : ''
+        };
+    
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+      handleSubmit(event) {
+        console.log("making request")
+        // fetch('/pneumonia-predict', {
+        //     method: 'POST',
+        //     body: data,
+        //   }).then((response) => {
+        //       response.json().then((body) => {
+        //       console.log(this.state);
+        //       console.log(body);
+        //       this.setState({ imageURL: body.file, submitted: true, result: body.result });
+        //       console.log(this.state);
+        //     });
+        //   });
+    //     fetch("/register", {
+    //         method:"POST",
+    //         cache: "no-cache",
+    //         body: data,
+    //         headers:{
+    //             "content_type":"application/json",
+    //         }
+    //         }
+    //     ).then(response => {
+    //     return response.json()
+    //   })
+    //   .then(json => {
+    //   this.setState({result: json[0]})
+    //   console.log(this.state.result)
+    //   })
+      }
     render() {
         return (
             <div className='inner-container'>
                <div className='header'>
                     Login
                 </div>
+                <form action='/login' method='POST' onSubmit={this.handleSubmit}>
                 <div className='box'>
-                <div className='input-group'>
-                    <label htmlFor='username'>
-                            Username
-                        </label>
-                    <input type='text' name='username' className='login-input' />
+                    <div className='input-group'>
+                        <label htmlFor='email'>
+                                Email ID
+                            </label>
+                        <input type='text' name='email' className='login-input' />
                     </div>
                     <div className='input-group'>
-                    <label htmlFor='password'>
-                            Password
-                        </label>
-                    <input type='password' name='password' className='login-input' />
+                        <label htmlFor='password'>
+                                Password
+                            </label>
+                        <input type='password' name='password' className='login-input' />
                     </div>
-<button type='button' className='login-btn' onClick={this.submitlogin.bind(this)}>Login</button>
-                </div>
+                    <button type='submit' className='login-btn' >Sign In</button>
+                    </div>
+                </form>
             </div>
         )
     }
--- src/components/pages/Register/Signup.js
+++ src/components/pages/Register/Signup.js
@@ -1,45 +1,80 @@
 import React, { Component } from 'react'
 import './style.css'
 class Signup extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-      }
+        this.state = {
+            result : ''
+        };
     
-    submitsignup(e){
-                }
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+      handleSubmit(event) {
+        console.log("making request")
+        // fetch('/pneumonia-predict', {
+        //     method: 'POST',
+        //     body: data,
+        //   }).then((response) => {
+        //       response.json().then((body) => {
+        //       console.log(this.state);
+        //       console.log(body);
+        //       this.setState({ imageURL: body.file, submitted: true, result: body.result });
+        //       console.log(this.state);
+        //     });
+        //   });
+    //     fetch("/register", {
+    //         method:"POST",
+    //         cache: "no-cache",
+    //         body: data,
+    //         headers:{
+    //             "content_type":"application/json",
+    //         }
+    //         }
+    //     ).then(response => {
+    //     return response.json()
+    //   })
+    //   .then(json => {
+    //   this.setState({result: json[0]})
+    //   console.log(this.state.result)
+    //   })
+      }
     render() {
         return (
             <div className='inner-container'>
                 <div className='header'>
                     Register
                 </div>
+                <form action='/register' method='POST' onSubmit={this.handleSubmit}>
                 <div className='box'>
-                <div className='input-group'>
-                    <label htmlFor='username'>
-                            Username
-                        </label>
-                    <input type='text' name='username' className='login-input' />
-                    </div>
-                    <div className='input-group'>
-                    <label htmlFor='email'>
-                            Email-id
-                        </label>
-                    <input type='text' name='email' className='login-input' />
-                    </div>
-                    <div className='input-group'>
-                    <label htmlFor='mobile'>
-                            Mobile No.
-                        </label>
-                    <input type='number' name='mobile' className='login-input' />
-                    </div>
-                    <div className='input-group'>
-                    <label htmlFor='password'>
-                            Password
-                        </label>
-                    <input type='password' name='password' className='login-input' />
-                    </div>
-<button type='button' className='login-btn' onClick={this.submitsignup.bind(this)}>Register</button>
+                        
+                        <div className='input-group'>
+                            <label htmlFor='name'>
+                                    Username
+                            </label>
+                            <input type='text' name='name' className='login-input' />
+                        </div>
+                        <div className='input-group'>
+                            <label htmlFor='email'>
+                                    Email-id
+                            </label>
+                            <input type='text' name='email' className='login-input' />
+                        </div>
+                        <div className='input-group'>
+                            <label htmlFor='mobile'>
+                                    Mobile No.
+                            </label>
+                            <input type='number' name='mobile' className='login-input' />
+                        </div>
+                        <div className='input-group'>
+                            <label htmlFor='password'>
+                                    Password
+                            </label>
+                            <input type='password' name='password' className='login-input' />
+                        </div>
+                            <button type='submit' className='login-btn' >Register</button>
+                      
                 </div>
+                </form>
             </div>
         )
     }
